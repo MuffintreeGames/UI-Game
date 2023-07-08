5d16964483bcd7df541104cd9ca8b639d07b4f57
nbirtch <nbirtch@gmail.com>
2023-07-08 16:58:51 -0700

    work on lever

--- Assets/Scripts/LeverState.cs
+++ Assets/Scripts/LeverState.cs
@@ -1,43 +1,68 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
-public class LeverState : MonoBehaviour
+public class LeverState : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     public bool topRegion = true;
     public static bool isValid = false;
     public ScoreManager scoreManager;
-    public SpriteRenderer spriteRenderer;
+    public Image spriteRenderer;
     public Sprite Up;
     public Sprite Down;
+    private bool mouse_over = false;
     // Start is called before the first frame update
     void Start()
     {
-        spriteRenderer = transform.parent.gameObject.GetComponent<SpriteRenderer>();
+        spriteRenderer = transform.parent.gameObject.GetComponent<Image>();
     }
 
-    private void OnMouseOver()
+    void Update()
     {
-        if (topRegion)
+        if (mouse_over && topRegion)
         {
             print("top region");
-            if (Input.GetMouseButton(0)) // Returns true for every frame that the mouse is being pressed.
+            if (Input.GetMouseButton(0)) {
+                // Returns true for every frame that the mouse is being pressed.
+                print("isValid = true");
                 isValid = true;
+                }
             else
-                isValid = false;
+            {
+                // isValid = false;
+                // print("isValid = false");
+            }
         }
-        else
+        else if (mouse_over && !topRegion)
         {
             print("bottom region");
             if (isValid && Input.GetMouseButtonDown(0))
             {
+                print("sprite down");
                 spriteRenderer.sprite = Down;
             }
-            else if (Input.GetMouseButtonUp(0)) {
+            else if (Input.GetMouseButtonUp(0))
+            {
                 // play sound effect
+                print("sprite up");
                 spriteRenderer.sprite = Up;
                 scoreManager.Reset();
+                isValid = false;
             }
         }
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        mouse_over = true;
+        Debug.Log("Mouse enter");
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        mouse_over = false;
+        Debug.Log("Mouse exit");
     }
 }
