7ad0c422df9f53cb748a830e8630f1e67369aebc
josephmeffen-code <josephmeffen@gmail.com>
2023-07-08 22:43:49 -0700

    ready screen, timer fix

--- Assets/Scripts/TimerWindupController.cs
+++ Assets/Scripts/TimerWindupController.cs
@@ -9,6 +9,7 @@ public class TimerWindupController : MonoBehaviour, IPointerEnterHandler, IPoint
     public int currentSection = 0;
     public static int lastSection = 0;
     public static bool isValid = false;
+    public static int consecutiveSections = 0;
     private bool mouse_over = false;
     public AudioSource windupSound;
     // Start is called before the first frame update
@@ -19,7 +20,11 @@ public class TimerWindupController : MonoBehaviour, IPointerEnterHandler, IPoint
     void Update()
     {
         if (!mouse_over) return;
-        if (!isValid) lastSection = 0;
+        //if (!isValid) lastSection = 0;
+        if (Input.GetMouseButtonDown(0))
+        {
+            lastSection = currentSection - 1;
+        }
         if (currentSection == lastSection + 1 && Input.GetMouseButton(0))
         {
             isValid = true;
@@ -27,24 +32,38 @@ public class TimerWindupController : MonoBehaviour, IPointerEnterHandler, IPoint
         }  else
         {
             isValid = false;
+            consecutiveSections = 0;
         }
-        if (isValid && currentSection == 4) { 
+        /*if (isValid && currentSection == 4) { 
                 isValid = false;
                 TimerController.AddTime();
                 windupSound.Play();
-        }
+        }*/
     }
 
     public void OnPointerEnter(PointerEventData eventData)
     {
         mouse_over = true;
         Debug.Log("Mouse enter");
-        if (!Input.GetMouseButton(0)) isValid = false;
+        if (!Input.GetMouseButton(0)) { isValid = false; consecutiveSections = 0; }
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
         mouse_over = false;
         lastSection = currentSection;
+        if (lastSection == 4)
+        {
+            lastSection = 0;
+        }
+        if (isValid)
+        {
+            consecutiveSections++;
+            if (consecutiveSections == 4) {
+                consecutiveSections = 0;
+                TimerController.AddTime();
+                windupSound.Play();
+            }
+        }
     }
 }
